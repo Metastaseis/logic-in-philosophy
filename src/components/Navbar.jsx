3f386cd00bd37109734378a31f4fe1448c6f12a8
Metastaseis <136683007+Metastaseis@users.noreply.github.com>
2025-08-07 07:13:45 +0000

    Remove duplicate Marquee import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import LanguageSwitcher from "./LanguageSwitcher";
 import { useTranslation } from "react-i18next";
 import Marquee from "react-fast-marquee";
-import Marquee from "react-fast-marquee";
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
